4a208e294ed51c1ce6d12f1b9586bd9d621f95d5
Alex Sim <alex.sim@blueprintprep.com>
2025-01-04 18:34:45 +0000

    adds prism syntax highlighting

--- wp-content/themes/editor-child/functions.php
+++ wp-content/themes/editor-child/functions.php
@@ -1,2 +1,21 @@
 <?php
-//Code goes here
+// Function to add prism.css and prism.js to the site
+function add_prism() {
+
+    // Register prism.css file
+    wp_register_style(
+        'prismCSS', // handle name for the style
+        get_stylesheet_directory_uri() . '/lib/prism.css' // location of the prism.css file
+    );
+
+    // Register prism.js file
+    wp_register_script(
+        'prismJS', // handle name for the script
+        get_stylesheet_directory_uri() . '/lib/prism.js' // location of the prism.js file
+    );
+
+    // Enqueue the registered style and script files
+    wp_enqueue_style('prismCSS');
+    wp_enqueue_script('prismJS');
+}
+add_action('wp_enqueue_scripts', 'add_prism');
