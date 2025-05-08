57659d6e9b80a006e435ccbd464559edcb3836ca
Alex Sim <alex.sim@blueprintprep.com>
2025-05-08 14:02:06 +0100

    adds new deploy mechanism

---
+++ deploy.php
@@ -0,0 +1,52 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Config
+set('keep_releases', 5);
+set('rsync_src', __DIR__);
+set('release_path','/var/www/alexsimcap');
+
+add('shared_files', ['wp-config.php']);
+add('shared_dirs', ['wp-content/uploads']);
+add('writable_dirs', []);
+
+// Hosts
+// Set an identitykey in /home/user/.ssh/config or ~/.ssh/config
+host('prod.alexsim.co.uk')
+    ->set('remote_user', 'deploy')
+    ->set('hostname', 'prod.alexsim.co.uk')
+    ->set('port', 22)
+    ->set('deploy_path','{{release_path}}')
+    ->set('rsync_src', '{{rsync_src}}')
+    ->set('rsync_dest','{{release_path}}');
+
+// Override deploy - to use rsync
+task('deploy:update_code', function () {
+    writeln("<info>Uploading files to server</info>");
+    upload(__DIR__ . "/", '{{deploy_path}}' . "/releases/" . "{{release_name}}", ["options" => ["--exclude-from=deployment-exclude-list.txt"]]);
+});
+
+// Override symlink - to point to correct folder
+task('deploy:shared', function () {
+    $sharedPath = "{{deploy_path}}/shared";
+    // Symlink Folders
+    foreach (get('shared_dirs') as $dir) {
+        run("{{bin/symlink}} $sharedPath/$dir {{deploy_path}}/releases/{{release_name}}/$dir");
+    }
+    // Symlink Files
+    foreach (get('shared_files') as $file) {
+        run("{{bin/symlink}} $sharedPath/$file {{deploy_path}}/releases/{{release_name}}/$file");
+    }
+
+});
+
+// Tasks
+task('deploy', [
+]);
+
+// Hooks
+// Before
+// After
+after('deploy:failed', 'deploy:unlock');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '945b84e006f9cdd855da50d8b416d04da8b2a009',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '945b84e006f9cdd855da50d8b416d04da8b2a009',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'deployer/deployer' => array(
+            'pretty_version' => 'v7.5.12',
+            'version' => '7.5.12.0',
+            'reference' => 'efc71dac9ccc86b3f9946e75d50cb106b775aae2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../deployer/deployer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
